69882245cafaf58e4ad49fa2f6d692c18929e8f3
Marudhu Pandiyan <29917@ITCINFOTECH.COM>
2018-11-08 15:56:26 +0530

    Email service config

--- src/main/java/com/boot/employee/service/EmailService.java
+++ src/main/java/com/boot/employee/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.boot.employee.service;
 
+import java.util.Date;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -14,45 +17,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 	
+	private String FROM_MAIL_ID = "marudhu.pandiyan@gmail.com";
+	private String SMTP_SERVER_HOST = "smtp.gmail.com";
+	private Integer SMTP_SERVER_PORT = 587;
+	private String FROM_MAIL_PWD = "_marudhu_ui";
+	private Boolean MAIL_SMTP_STARTTLS_ENABLE = true;
+	private String Mail_content = "Erste registration is done succesfully . Please set your password using this link HERE";
 	public boolean sendEmail(String ToEmailId) {
 		
-		// Assuming you are sending email from localhost
-	      String host = "localhost";
-	      String username = "marudhu.pandiyan@itcinfotech.com";
-	      String password = "pandi@123";
-	      // Get system properties
-	      Properties properties = System.getProperties();
-	    //  properties.setProperty("mail.user", username);
-	      // Setup mail server
-	     // properties.setProperty("mail.smtp.host", host);
-
-	      // Get the default Session object.
-	      Session session = Session.getDefaultInstance(properties);
-	      
 		try {
 	        // Create a default MimeMessage object.
-	        MimeMessage message = new MimeMessage(session);
-
-	        // Set From: header field of the header.
-	        message.setFrom(new InternetAddress(username));
-
-	        // Set To: header field of the header.
-	        message.addRecipient(Message.RecipientType.TO, new InternetAddress(ToEmailId));
-
-	        // Set Subject: header field
+	        MimeMessage message = new MimeMessage(getSession());
+	        message.setFrom(new InternetAddress(FROM_MAIL_ID));
+	        message.setSentDate(new Date());
+	        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ToEmailId));
 	        message.setSubject("Erste Registration Mail!");
-
-	        // Now set the actual message
-	        message.setText("Erste registration is done succesfully . Please set your password using this link HERE");
+	        message.setContent(Mail_content, "text/html");
 
 	        // Send message
-	        Transport t = session.getTransport("smtps");
-	        try {
-	            t.connect(host, username, password);
-	            t.sendMessage(message, message.getAllRecipients());
-	        } finally {
-	            t.close();
-	        }
+	        Transport.send(message);
 	        System.out.println("Sent message successfully....");
 	        return true;
 	     } catch (MessagingException mex) {
@@ -61,4 +44,19 @@ public class EmailService {
 	     }
 	}
 	
+	private Session getSession() {
+        Properties props = new Properties();
+        props.put("mail.smtp.from", FROM_MAIL_ID);
+        props.put("mail.smtp.host", SMTP_SERVER_HOST);
+        props.put("mail.smtp.port", SMTP_SERVER_PORT);
+        props.put("mail.smtp.starttls.enable", MAIL_SMTP_STARTTLS_ENABLE);
+        props.put("mail.smtp.auth", true);
+
+        return Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM_MAIL_ID, FROM_MAIL_PWD);
+            }
+        });
+    }
 }
